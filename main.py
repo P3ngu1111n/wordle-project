43cfdf520d0336decb026e2407d68d6e798f3423
Your Name <you@example.com>
2026-05-01 19:33:51 +0100

    just adding in geometry and a window title, nothing relly done, May 1st Friday

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
+from tkinter import ttk
 
 wordle_window = Tk() #window instantion 
+wordle_window.geometry("400x400")
+wordle_window.title("Wordle Clone Type shii")
 
 wordle_window.mainloop()
