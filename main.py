6d6acdcd6be3b55481aa2ad02063594c9a2b894b
Your Name <you@example.com>
2026-05-02 13:53:05 +0100

    added in the "title" and gave it that vomit green look

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 
+#Everything about the window
 wordle_window = Tk() #window instantion 
-wordle_window.geometry("400x400")
-wordle_window.title("Wordle Clone Type shii")
+wordle_window.geometry("400x200")
+wordle_window.title("Wordle Clone Type shii") # not useful for linux hyprland so cant test if this works proper
 
+label = Label(wordle_window,text="wordle clone",font=('Satoshi',20,'bold'),fg='#00FF00')
+label.pack()
+
+#Run the window 
 wordle_window.mainloop()
