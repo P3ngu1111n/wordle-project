d958aa4217247607e4a5df9b83ff0c61dd8b4c03
Your Name <you@example.com>
2026-05-02 14:38:00 +0100

    fixed naming convention

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ wordle_window = Tk() #window instantion
 wordle_window.geometry("400x200")
 wordle_window.title("Wordle Clone Type shii") # not useful for linux hyprland so cant test if this works proper
 
-label = Label(wordle_window,text="wordle clone",font=('Satoshi',20,'bold'),fg='#00FF00')
-label.pack()
+#wordle title
+wordle_header = Label(wordle_window,
+text="wordle clone",
+font=('Satoshi',20,'bold'),
+fg='#00FF00',
+relief=RAISED, #border type 
+bd=10, #border thiccness
+)
+
+
+wordle_header.pack()
 
 #Run the window 
 wordle_window.mainloop()
