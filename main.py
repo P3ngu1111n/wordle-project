b431503ac2e3ff2dda9b45446d63b57b03850432
Your Name <you@example.com>
2026-05-03 02:19:14 +0100

    old requirements was  directily from my pc so its been fixed and the check button has been added

--- main.py
+++ main.py
@@ -7,16 +7,26 @@ wordle_window.geometry("400x200")
 wordle_window.title("Wordle Clone Type shii") # not useful for linux hyprland so cant test if this works proper
 
 #wordle title
-wordle_header = Label(wordle_window,
-text="wordle clone",
+wordle_header = Label(
+wordle_window,
+text="Wordle Clone",
 font=('Satoshi',20,'bold'),
 fg='#00FF00',
-relief=RAISED, #border type 
-bd=10, #border thiccness
+# relief=RAISED, #border type 
+# bd=10, #border thiccness
 )
-
-
 wordle_header.pack()
+
+#buttons 
+check_button = Button(
+wordle_window,
+text="check",
+font=('Roboto',10),
+pady=2 ,
+# command= , # a function call back goes in here 
+)
+check_button.pack()
+
 
 #Run the window 
 wordle_window.mainloop()
