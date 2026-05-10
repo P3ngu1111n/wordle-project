3c4e293c41ac269a2069363486fb1a0576aef0ec
Your Name <you@example.com>
2026-05-10 20:09:24 +0100

    added user input and the random 5 character word creator added wonderwords library made the user input readable from the function and added a regenerate button

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from tkinter import *
 from tkinter import ttk
+from wonderwords import RandomWord
 
 
 
-#Everything about the window
+#Window creation
 wordle_window = Tk() #window instantion 
 wordle_window.geometry("400x200")
-
-
-#user input 
-user_input = Entry()
-user_input.config(font=("sans-serif"),)
-user_input.pack()
-
 
 
 #wordle title
@@ -26,25 +20,47 @@ fg='#00FF00',
 )
 wordle_header.pack()
 
+#user input 
+user_input = Entry()
+user_input.config(font=("sans-serif"),)
+user_input.pack()
+
+
+def regnrt() :
+    r = RandomWord()
+    rword =  r.word(word_min_length=5, word_max_length=5 )
+    print (rword)
+    return rword
+
 
 
 def submit() :
     user_guess = user_input.get()
     print (user_guess)
+    return user_guess
+
 
 
 #buttons 
 check_button = Button(
+
 wordle_window,
 text="check",
 font=('Roboto',10),
 pady=2 ,
 command = submit, # a function call back goes in here 
 )
+
+
+gnrt_button = Button(
+    text="regenerate",
+    font=('Roboto',10),
+    pady=2 ,
+    command = regnrt, 
+)
+
 check_button.pack()
-
-
-
+gnrt_button.pack()
 
 #Run the window 
 wordle_window.mainloop()
